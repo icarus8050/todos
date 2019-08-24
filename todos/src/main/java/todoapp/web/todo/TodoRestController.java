0c65ad915601334e2d0b703384d929c78d838f49
icarus8050 <icarus8050@naver.com>
2019-08-24 16:22:01 +0900

    create api

--- todos/src/main/java/todoapp/web/todo/TodoRestController.java
+++ todos/src/main/java/todoapp/web/todo/TodoRestController.java
@@ -2,23 +2,50 @@ package todoapp.web.todo;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import todoapp.core.todos.application.TodoEditor;
 import todoapp.core.todos.application.TodoFinder;
 import todoapp.core.todos.domain.Todo;
 
 @RestController
 public class TodoRestController {
 	
-	private TodoFinder finder;
+	private final Logger log = LoggerFactory.getLogger(TodoRestController.class);
 	
-	public TodoRestController(TodoFinder finder) {
+	private TodoFinder finder;
+	private TodoEditor editor;
+	
+	public TodoRestController(TodoFinder finder, TodoEditor editor) {
 		this.finder = finder;
+		this.editor = editor;
 	}
 	
 	@GetMapping("/api/todos")
 	public List<Todo> todos() {
 		return finder.getAll();
+	}
+	
+	@PostMapping("/api/todos")
+	public void create(@RequestBody TodoWriteCommand command) {
+		log.debug("command.title: {}", command.getTitle());
+		editor.create(command.getTitle());
+	}
+	
+	public static class TodoWriteCommand {
+		private String title;
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
 	}
 }
